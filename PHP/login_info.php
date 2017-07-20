68f6618fa621a010d1c3ed34ae89dfc124518baf
yejingyu <yejingyu@users.noreply.github.com>
2017-07-20 03:54:29 -0700

    Add files via upload
    first upload

---
+++ PHP/login_info.php
@@ -0,0 +1,13 @@
+<!-- 
+# Copyright © 2017 Jingyu Ye
+# This program is licensed under the MIT License.
+# Please see the file LICENSE in this distribution for
+# license terms.
+-->
+
+<?php
+$username = "Your database username";
+$password = 'Your database password';
+$databasename = "Your database name";
+$hostname = "The name of your host";
+?>
---
+++ PHP/manager.php
@@ -0,0 +1,26 @@
+<!-- 
+# Copyright © 2017 Jingyu Ye
+# This program is licensed under the MIT License.
+# Please see the file LICENSE in this distribution for
+# license terms.
+-->
+
+<html>
+<head>
+</head>
+<body>
+<h1>Manager Page</h1>
+
+
+<h2>Menu Editing</h2>
+<h3>add item</h3>
+<!– get input –>
+<form action="add_food.php" method="post">
+Name: <input type="text" name="name" /><br><br>
+Price: <input type="text" name="price" /><br><br>
+
+ 
+<input type="submit" />
+</form>
+</body>
+</html>
