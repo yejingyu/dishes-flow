fdec25ff89677cb138ffc0368430e47ae41283ba
yejingyu <yejingyu@users.noreply.github.com>
2017-08-18 14:54:32 -0700

    Add files via upload

--- PHP/waitstaff/add_order.php
+++ PHP/waitstaff/add_order.php
@@ -7,16 +7,24 @@
 
 <?php
 include '../login_info.php'; //login information
+include '../public_function.php'; //helper functions
 $connection = pg_connect("host=$hostname dbname=$databasename user=$username password=$password")
   or die ("Could not connect");
 
 //insert
-$result = pg_query_params($connection,
+if(in_stock($connection, "menu", $_REQUEST[foodid]) == "t")
+{
+  $result = pg_query_params($connection,
   'INSERT INTO order_list (table_ID,food_ID,quantity,status,comment) VALUES ($1,$2,$3,$4,$5)',
   array($_REQUEST[tableid], $_REQUEST[foodid], $_REQUEST[quantity], "Ordered", $_REQUEST[comment]))
   or die("Query error:" . pg_last_error());
-//return success information
-echo "Insert successful, one record added";
+  //return success information
+  echo "Insert successful, one record added";
+}
+else
+{
+  echo "The food you order is not	available now!";
+}
  
 pg_close($connection);
 ?>
